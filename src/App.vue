89fcd968d99cbd334ef6d7b4fdc857b38ccd3b7c
wuhen <1738100406@qq.com>
2020-12-27 20:41:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home1</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>home</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  methods: {},
+};
+</script>
